c6434425b44d12886c1ee813d8bdbbfc217d49c3
Jennifer Christos <jennifer.christos3@gmail.com>
2014-12-14 12:20:43 -0500

    Added ability to use secret agent name

--- js/questly.js
+++ js/questly.js
@@ -1,10 +1,49 @@
 // Connected to index.html
 function questly() {
-  var myForm, nameField, questField, colorField, userData;
+  var myForm, nameField, secretAgentNameField, questField, colorField, userData;
   myForm = document.querySelector('form');
   nameField = myForm.elements.name;
+  secretAgentNameField = myForm.elements.secret_agent_name;
   questField = myForm.elements.quest;
   colorField = myForm.elements.color;
+
+  function getValues(ev) {
+    ev.preventDefault();
+    //debugger;
+    userData = {
+      name: nameField.value,
+      secretAgentName: secretAgentNameField.value,
+      quest: questField.value,
+      color: colorField.value,
+      friend: this.elements.friend.checked,
+      useAgent: this.elements.use_agent.checked
+    }
+    //console.log(userData.friend);
+    writeData();
+  }
+
+  function writeData() {
+    var storyDiv = document.getElementById('story');
+    var friendStory = '';
+    if (userData.friend) {
+      friendStory = ' Hey, I see you brought your friend!';
+    }
+    var nameToUse = userData.name;
+    if (userData.useAgent) {
+      nameToUse = userData.secretAgentName;
+    }
+    var story = nameToUse + ', in a quest to ' + userData.quest + ', shall ride a ' + userData.color + ' horse.' + friendStory;
+
+    storyDiv.innerHTML = story;
+  }
+
+myForm.onsubmit = getValues;
+
+}
+questly();
+
+
+
 
 
 // function concat(str1, str2) {
@@ -13,33 +52,6 @@ function questly() {
 
 // concat('Kermit', 'The Frog');
 // will return 'Kermit The Frog'
-
-function getValues(ev) {
-  ev.preventDefault();
-  //debugger;
-  userData = {
-    name: nameField.value,
-    quest: questField.value,
-    color: colorField.value,
-    friend: this.elements.friend.checked
-  }
-  //console.log(userData.friend);
-  writeData();
-}
-
-function writeData() {
-  var storyDiv = document.getElementById('story');
-  var friendStory = '';
-  if (userData.friend) {
-    friendStory = ' Hey, I see you brought your friend!';
-  }
-  storyDiv.innerHTML = userData.name + ', in a quest to ' + userData.quest + ', shall ride a ' + userData.color + ' horse.' + friendStory;
-}
-
-myForm.onsubmit = getValues;
-
-}
-questly();
 
 // access values like this, once you've run getValues():
 // userData.quest
