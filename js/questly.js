90f3270b1ea6b6409c1e6fd1b777592e6e5133d2
Jennifer Christos <jennifer.christos3@gmail.com>
2014-12-13 20:44:35 -0500

    Write some functions.

--- js/questly.js
+++ js/questly.js
@@ -1,53 +1,32 @@
-// var name = prompt('What is your name?');
-//
-// if (name && (name !=='null')) {
-//     document.writeln('Go on, ');
-//   }
-//   else {
-//     document.writeln('<p>Oops, start over</p>');
+// Connected to index.html
+
+var myForm, nameField, questField, userData;
+myForm = document.querySelector('form');
+nameField = myForm.elements.name;
+questField = myForm.elements.quest;
+
+// function concat(str1, str2) {
+//   return str1 + ' ' + str2;
 // }
-// var quest = prompt('What is your quest?');
-//
-// if (quest && (quest !=='null')) {
-//     document.writeln('proceed...');
-//   }
-//   else {
-//     document.writeln('<p>Oops, start over</p>');
-// }
-// var color = prompt('What is your favorite color?');
-//
-// if (color && (color !=='null')) {
-//     document.writeln('Very good my friend.');
-//   }
-//   else {
-//     document.writeln('<p>Oops, start over</p>');
-// }
-// alert(name + ' is a knight and shall be granted a ' + color + ' horse. This be the quest of the knight: ' + quest + '.')
-//
 
+// concat('Kermit', 'The Frog');
+// will return 'Kermit The Frog'
 
-
-
-
-
-//Lab time!
-//
-// Go full Monty Python! Ask "What is your name?", "What is your quest?", and "What is your favorite color". Write something to the document using all three of the entered values.
-
-
-var name = prompt('What is your name?');
-if (!name || name === 'null') {
-  name = 'Stupidhead';
+function getValues(ev) {
+  ev.preventDefault();
+  userData = {
+    name: nameField.value,
+    quest: questField.value
+  }
+  writeData();
 }
 
-var quest = prompt('What is your quest?');
-if (!quest || quest === 'null') {
-  quest = 'To become more cooperative';
+function writeData() {
+  var storyDiv = document.getElementById('story');
+  storyDiv.innerHTML = userData.name + ', ' + userData.quest;
 }
 
-var color = prompt('What is your favorite color?')
-if (!color || color === 'null') {
-  color = 'clear';
-}
+myForm.onsubmit = getValues;
 
-document.writeln('<p>There once was a warrior named ' + name + ' on a mighty quest indeed: ' + quest + '. Luckily, ' + name + ' was wearing lucky socks&mdash;colored ' + color + '<p>');
+// access values like this, once you've run getValues():
+// userData.quest
